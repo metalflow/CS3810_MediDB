a42a8bc007056318c0553dac6d775b54cb966ec7
os fuscas abandonado <os fuscas abandonado@DOMINGOS>
2016-11-28 20:04:27 -0700

    added inserts / testing views / scrap files

---
+++ SQLscripts/insert into employee - select view.sql
@@ -0,0 +1,5 @@
+insert into employee 
+	values (24020043, '888ball', 'joe@sha.com', 21312414, 'doctor', 1, 0);
+    
+select * from all_employees;
+
---
+++ SQLscripts/insert into location - select view.sql
@@ -0,0 +1,4 @@
+insert into location
+	values (113, 'silent hill', 123, 21397123, null, null);
+
+select * from all_locations;
